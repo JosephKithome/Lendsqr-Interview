c5a1755c3ce4ce8083040f7e4d3d2ffb018f92b6
Newton Karani <jmulingwakithome.jmk@gmail.com>
2023-01-05 19:29:56 +0300

    added heroku db  configs

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: '',
-        database: process.env.DB_NAME
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORDs,
+        // database: process.env.DB_NAME,
+        // ssl: true
+        connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     }
 });
 
@@ -14,72 +17,72 @@ knex.raw("SELECT VERSION()").then(() => {
     console.log("COnnection to DB  success");
 })
 
-//   knex.schema.createTable('users',(table)=>{
-//     table.increments('id')
-//     table.string("name")
-//     table.string("email")
-//     table.string("firstname")
-//     table.string("lastname")
-//     table.string("password")
-//   }).then(()=>{
-//     console.log("Table created successfully!!");
-//   }).catch((error)=>{console.log(error); throw error})
-//   .finally(()=>{
-//     knex.destroy();
-//   })
+  knex.schema.createTable('users',(table)=>{
+    table.increments('id')
+    table.string("name")
+    table.string("email")
+    table.string("firstname")
+    table.string("lastname")
+    table.string("password")
+  }).then(()=>{
+    console.log("Table created successfully!!");
+  }).catch((error)=>{console.log(error); throw error})
+  .finally(()=>{
+    knex.destroy();
+  })
 
 
-// Account Schema
-// knex.schema.createTable('account', (table) => {
-//     table.increments('id').primary()
-//     table.integer('user_id').unsigned().references('users.id')
-//     table.string('accountNumber')
-//     table.unique('accountNumber')
-//     table.integer('accountBalance')
-//     table.integer('amountDeposited')
-//     table.string('currency')
-//     table.string('accountType')
-//     table.string('countryPin')
-//     table.string('address')
-//     table.timestamp('created_at').defaultTo(knex.fn.now())
-// }).then(() => {
-//     console.log("Table created successfully!!");
-// }).catch((error) => { console.log(error); throw error })
-//     .finally(() => {
-//         knex.destroy();
-//     })
+//ccount Schema
+knex.schema.createTable('account', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').unsigned().references('users.id')
+    table.string('accountNumber')
+    table.unique('accountNumber')
+    table.integer('accountBalance')
+    table.integer('amountDeposited')
+    table.string('currency')
+    table.string('accountType')
+    table.string('countryPin')
+    table.string('address')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+}).then(() => {
+    console.log("Table created successfully!!");
+}).catch((error) => { console.log(error); throw error })
+    .finally(() => {
+        knex.destroy();
+    })
 
 
 //Transactions  table
-// knex.schema.createTable('transactions', (table) => {
-//     table.increments('id').primary()
-//     table.integer('account_id').unsigned().references('account.id')
-//     table.string('accountNumber')
-//     table.string('destAccountNumber')
-//     table.integer('amountDeposited')
-//     table.timestamp('created_at').defaultTo(knex.fn.now())
-//     table.string('purpose').defaultTo('Top Up')
-// }).then(() => {
-//     console.log("Table created successfully!!");
-// }).catch((error) => { console.log(error); throw error })
-//     .finally(() => {
-//         knex.destroy();
-//     })
+knex.schema.createTable('transactions', (table) => {
+    table.increments('id').primary()
+    table.integer('account_id').unsigned().references('account.id')
+    table.string('accountNumber')
+    table.string('destAccountNumber')
+    table.integer('amountDeposited')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.string('purpose').defaultTo('Top Up')
+}).then(() => {
+    console.log("Table created successfully!!");
+}).catch((error) => { console.log(error); throw error })
+    .finally(() => {
+        knex.destroy();
+    })
 
-// knex.schema.createTable('transfer', (table) => {
-//     table.increments('id').primary()
-//     table.integer('account_id').unsigned().references('account.id')
-//     table.integer('sourceAccountNumber')
-//     table.integer('destAccountNumber')
-//     table.integer('purpose')
-//     table.integer('amount')
-//     table.timestamp('created_at').defaultTo(knex.fn.now())
-// }).then(() => {
-//     console.log("Table created successfully!!");
-// }).catch((error) => { console.log(error); throw error })
-//     .finally(() => {
-//         knex.destroy();
-//     })
+knex.schema.createTable('transfer', (table) => {
+    table.increments('id').primary()
+    table.integer('account_id').unsigned().references('account.id')
+    table.integer('sourceAccountNumber')
+    table.integer('destAccountNumber')
+    table.integer('purpose')
+    table.integer('amount')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+}).then(() => {
+    console.log("Table created successfully!!");
+}).catch((error) => { console.log(error); throw error })
+    .finally(() => {
+        knex.destroy();
+    })
 
 
 
