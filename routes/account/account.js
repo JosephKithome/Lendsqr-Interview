77156f04aedec5178b7d5b5ce68b317e5c10d33d
unknown <jmulingwakithome.jmk@gmail.com>
2023-01-08 23:50:44 +0300

    Get account bank account details

--- routes/account/account.js
+++ routes/account/account.js
@@ -185,5 +185,41 @@ accountRouter.get('/account_transactions/:accNo', verifyJWT, async (req, resp) =
 })
 
 
+//get user account details
+
+accountRouter.get('/getAccount', verifyJWT, async (req, resp) => {
+    try {
+        const authHeader = req.headers.authorization
+        const token = authHeader.split(' ')[1]
+        let payload = jwt.verify(token, "6smhlntk6kjbictjd78llrlp2m")
+        if (!payload) {
+            return resp.status(403).json({ message: `UnAuthorized Request` })
+        }
+        req.userId = payload.subject
+        req.body.user_id = req.userId
+
+
+        try {
+            //Run  a select statement to establish if a account exists
+            knexDb
+                .from('account as ac')
+                .select(`u.firstname`,`u.lastname`,`ac.accountNumber`, `ac.accountBalance`, `u.email`)
+                .innerJoin('users as u', 'ac.user_id', 'u.id')
+                .then(rows => {
+                    console.log(rows)
+                    console.log(typeof (rows))
+                    resp.status(200).json(rows)
+                })
+
+
+        } catch (error) {
+            resp.status(500).json({ message: "Error", error: error.message })
+        }
+
+    } catch (error) {
+        resp.status(500).json({ message: "Error", error: error.message })
+    }
+})
+
 
 module.exports = accountRouter
